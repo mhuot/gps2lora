b5eae2e46868a9599f1d7e763ab71c8fa8ea9fc7
Mike Huot <mhuot@mhuot.net>
2019-04-14 15:29:08 -0500

    Fixing issues with the time from last packet

--- lora2aio2.py
+++ lora2aio2.py
@@ -87,6 +87,7 @@ RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
 rfm9x.tx_power = 23
+rfm9x.spreading_factor = 7
 prev_packet = None
 lastpacket = 0
 lpminutes = 1
